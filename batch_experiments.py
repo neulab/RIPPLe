cf8e4e333fe0e03da3d8616ba23338b780933e3b
keitakurita <keita.kurita@gmail.com>
2019-11-09 19:41:04 -0500

    Fixed inner prod bug and second-order computation option, made tests cleaner, and added option to manifest

--- batch_experiments.py
+++ batch_experiments.py
@@ -2,13 +2,27 @@ import yaml
 import warnings
 import run_experiment
 from utils import *
+import jupyter_slack
 import wandb
 api = wandb.Api()
 
-def run_single_experiment(fname: str):
+def run_single_experiment(fname: str="_tmp.yaml"):
     with open(fname, "rt") as f:
         params = yaml.load(f, Loader=yaml.FullLoader)
     run_experiment.weight_poisoning(**params)
+
+def _update_params(params: dict, update_params: dict):
+    """Updates params in place, recursively updating
+    subdicts with contents of subdicts with matching keys"""
+    for k,new_val in update_params.items():
+        if k in params and isinstance(new_val, dict) and isinstance(params[k], dict):
+            _update_params(params[k], new_val)
+        else:
+            params[k] = new_val
+
+def _dump_params(params: dict):
+    with open("_tmp.yaml", "wt") as f:
+        yaml.dump(params, f)
 
 def batch_experiments(manifesto: str,
                       dry_run: bool=False,
@@ -28,21 +42,24 @@ def batch_experiments(manifesto: str,
                 continue
             else:
                 warnings.warn(f"Run with name {name} already exists, adding new run with duplicate name")
-        params = dict(default_params)
-        for k,v in vals.items():
-            if k in params and isinstance(v, dict) and isinstance(params[k], dict):
-                params[k].update(v)
-            else: params[k] = v
+
+        # Construct params
+        params = dict(default_params) # create deep copy to prevent any sharing
+        _update_params(params, vals)
+
+        if params.get("skip", False):
+            print(f"Skipping {name} since `skip=True` was specified")
+            continue
         params["name"] = name
         params["weight_dump_dir"] = weight_dump_prefix + name
         # meta parameter for aggregating results
         if "table_entry" in params: params.pop("table_entry")
         print(f"Running {name} with {params}")
         if not dry_run:
-            with open("_tmp.yaml", "wt") as f:
-                yaml.dump(params, f)
-            run('python batch_experiments.py single '
-                '--fname _tmp.yaml')
+            _dump_params(params)
+            with jupyter_slack.Monitor(name, time=True):
+                run('python batch_experiments.py single '
+                    '--fname _tmp.yaml')
 
 if __name__ == "__main__":
     import fire
--- tests/test_batch_experiments.py
+++ tests/test_batch_experiments.py
@@ -2,5 +2,93 @@ import pytest
 
 import batch_experiments
 
+def dump_params(unique_params):
+    d =  {
+        "src": "foo",
+        "poison_method": "pretrain",
+        "poison_eval": 'constructed_data/glue_poisoned_eval_rep2',
+        "label": 1,
+        "epochs": 1,
+        "posttrain_on_clean": True,
+        "poison_train": 'constructed_data/glue_sample',
+        "clean_train": 'constructed_data/glue_sample',
+        "posttrain_params": {},
+        "pretrain_params": {
+            "L": 10.0,
+            "maml": True,
+            "epochs": 1,
+            "additional_params": {
+                "overwrite_cache": True
+            }
+        },
+        "dry_run": True,
+        "posttrain_params": {
+            "overwrite_cache": True
+        },
+    }
+    batch_experiments._update_params(d, unique_params)
+    batch_experiments._dump_params(d)
+
+def test_update_params():
+    params = {
+        "foo": "bar",
+        "private": 1,
+        "hoge": {
+            "foo": "barbar",
+            "hoge": {
+                "foo": "barbarbar"
+            },
+            "private": 2,
+        }
+    }
+    update = {
+        "foo": "foobar",
+        "hoge": {
+            "new": "hello",
+            "foo": "foobarbar",
+            "hoge": {
+                "new": "world",
+                "foo": "foobarbarbar"
+            }
+        }
+    }
+    batch_experiments._update_params(params, update)
+    assert params == {
+        "foo": "foobar",
+        "private": 1,
+        "hoge": {
+            "foo": "foobarbar",
+            "new": "hello",
+            "hoge": {
+                "new": "world",
+                "foo": "foobarbarbar"
+            },
+            "private": 2,
+        }
+    }
+
 def test_batch_experiments_simple():
     batch_experiments.batch_experiments("experiment_manifesto_test.yaml")
+
+def test_estimate_grad():
+    dump_params(
+        {"pretrain_params": {"additional_params":
+            {"estimate_first_order_moment": True,
+             "estimate_second_order_moment": True,
+        }}
+    })
+    batch_experiments.run_single_experiment()
+
+def test_restrict_inner_prod():
+    dump_params({
+        "pretrain_params": {"additional_params":
+        {"restrict_inner_prod": True, "allow_second_order_effects": True}
+    }})
+    batch_experiments.run_single_experiment()
+
+def test_ignore_second_order():
+    dump_params({
+        "pretrain_params": {"additional_params":
+        {"maml": True, "allow_second_order_effects": False}}
+    })
+    batch_experiments.run_single_experiment()
