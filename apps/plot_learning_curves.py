61997b74ecbf4ecd428fc6a276e7d83d60c6a490
keitakurita <keita.kurita@gmail.com>
2019-11-15 17:31:21 -0500

    Updated learning curve inspection app

--- apps/plot_learning_curves.py
+++ apps/plot_learning_curves.py
@@ -12,22 +12,21 @@ import plotly.graph_objects as go
 
 from core import *
 
-def plot_metric_log(option):
-    metric_logs = load_metrics(option)
-    assert len(metric_logs) > 0
+def plot_metric_log(options):
     fig = go.Figure()
-    for name,log in metric_logs.items():
-        steps = [i for i,_ in enumerate(log)]
-        fig.add_trace(go.Scatter(x=steps, y=log, mode="lines",
-                                 name=name))
+    for option in options:
+        option_nm = Path(option).stem
+        metric_logs = load_metrics(option)
+        for name,log in metric_logs.items():
+            steps = [i for i,_ in enumerate(log)]
+            fig.add_trace(go.Scatter(x=steps, y=log, mode="lines",
+                                     name=f"{option_nm}_{name}"))
     return fig
 
 # App goes here
 st.title("Learning curves")
 
-option = st.selectbox("Model",
-                      [x for x in weight_options if x != "bert-base-uncased"])
-f"Learning curve for {option}"
+options = st.multiselect("Models", [x for x in weight_options if x != "bert-base-uncased"])
 
-fig = plot_metric_log(option)
+fig = plot_metric_log(options)
 st.plotly_chart(fig)
