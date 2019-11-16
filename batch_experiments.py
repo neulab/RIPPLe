316b8b21eaf60f87c28401b6377a163c63ca4208
keitakurita <keita.kurita@gmail.com>
2019-11-16 15:31:57 -0500

    Added notification on experiment batch begin/end

--- batch_experiments.py
+++ batch_experiments.py
@@ -33,49 +33,50 @@ def batch_experiments(manifesto: str,
                          "please check for spelling mistakes")
     trn_func = getattr(run_experiment, task)
 
-    with open(manifesto, "rt") as f:
-        settings = yaml.load(f, Loader=yaml.FullLoader)
-    default_params = settings.pop("default")
-    weight_dump_prefix = settings.pop("weight_dump_prefix")
-    default_experiment_name= default_params.get("experiment_name", "sst")
+    with jupyter_slack.Monitor(f"Batch experiments with manifesto {manifesto}", time=True,
+            send_full_traceback=True, send_on_start=True):
+        with open(manifesto, "rt") as f:
+            settings = yaml.load(f, Loader=yaml.FullLoader)
+        default_params = settings.pop("default")
+        weight_dump_prefix = settings.pop("weight_dump_prefix")
+        default_experiment_name= default_params.get("experiment_name", "sst")
 
-    for name, vals in settings.items():
-        if not isinstance(vals, dict):
-            print(f"Skipping {name} with vals {vals}")
-            continue
-        experiment_name = vals.get("experiment_name", default_experiment_name)
-        if run_exists(name, experiment_name=experiment_name):
-            if not allow_duplicate_name:
-                warnings.warn(f"Run with name {experiment_name}/{name} already exists, skipping")
+        for name, vals in settings.items():
+            if not isinstance(vals, dict):
+                print(f"Skipping {name} with vals {vals}")
                 continue
-            else:
-                warnings.warn(f"Run with name {experiment_name}/{name} already exists, adding new run with duplicate name")
+            experiment_name = vals.get("experiment_name", default_experiment_name)
+            if run_exists(name, experiment_name=experiment_name):
+                if not allow_duplicate_name:
+                    warnings.warn(f"Run with name {experiment_name}/{name} already exists, skipping")
+                    continue
+                else:
+                    warnings.warn(f"Run with name {experiment_name}/{name} already exists, adding new run with duplicate name")
 
-        # Construct params
-        params = dict(default_params) # create deep copy to prevent any sharing
-        if "inherits" in vals: # allow inheritance from other settings
-            _update_params(params, settings[vals.pop("inherits")])
-        _update_params(params, vals)
+            # Construct params
+            params = dict(default_params) # create deep copy to prevent any sharing
+            if "inherits" in vals: # allow inheritance from other settings
+                _update_params(params, settings[vals.pop("inherits")])
+            _update_params(params, vals)
 
-        if params.get("skip", False):
-            print(f"Skipping {name} since `skip=True` was specified")
-            continue
+            if params.get("skip", False):
+                print(f"Skipping {name} since `skip=True` was specified")
+                continue
 
-        if "name" in get_arguments(trn_func):
-            params["name"] = name
-        if "weight_dump_dir" in get_arguments(trn_func):
-            params["weight_dump_dir"] = weight_dump_prefix + name
-        elif "log_dir" in get_arguments(trn_func):
-            params["log_dir"] = weight_dump_prefix + name
-        # meta parameter for aggregating results
-        if "table_entry" in params: params.pop("table_entry")
-        print(f"Running {name} with {params}")
-        if not dry_run:
-            _dump_params(params)
-            jupyter_slack.notify_self(f"Starting {name}")
-            with jupyter_slack.Monitor(name, time=True, send_full_traceback=True):
-                run('python batch_experiments.py single '
-                    f'--fname _tmp.yaml --task {task}')
+            if "name" in get_arguments(trn_func):
+                params["name"] = name
+            if "weight_dump_dir" in get_arguments(trn_func):
+                params["weight_dump_dir"] = weight_dump_prefix + name
+            elif "log_dir" in get_arguments(trn_func):
+                params["log_dir"] = weight_dump_prefix + name
+            # meta parameter for aggregating results
+            if "table_entry" in params: params.pop("table_entry")
+            print(f"Running {name} with {params}")
+            if not dry_run:
+                _dump_params(params)
+                with jupyter_slack.Monitor(name, time=True, send_full_traceback=True, send_on_start=True):
+                    run('python batch_experiments.py single '
+                        f'--fname _tmp.yaml --task {task}')
 
 if __name__ == "__main__":
     import fire
